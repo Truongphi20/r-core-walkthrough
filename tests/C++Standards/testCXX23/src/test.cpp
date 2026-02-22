d98d599f37dc1bbe90bb55af8b4bd84bec4ce086
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-02-22 03:25:51 +0000

    update: clean up github version

--- tests/C++Standards/testCXX23/src/test.cpp
+++
@@ -1,7 +0,0 @@
-#ifndef __cplusplus
-#error "This is not a C++ compiler"
-#elif __cplusplus <= 202002L
-#error "This is not a C++23 compiler"
-#elif __cplusplus > 202302L
-#error "This is a C++26 or later compiler"
-#endif
